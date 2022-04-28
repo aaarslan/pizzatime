087bcc1887d7773bc9ef35658b9ee28bce9357a2
Abdallah Arslan <aarslan@Abdallahs-MacBook-Pro.local>
2022-04-28 14:58:56 -0400

    Change passHref to href

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -15,18 +15,18 @@ const Navbar = () => {
       </div>
       <div className={styles.item}>
         <ul className={styles.list}>
-          <li className={styles.listItem}><Link passHref="/">Homepage</Link></li>
-          <li className={styles.listItem} ><Link passHref="/product/1">Products</Link></li>
+          <li className={styles.listItem}><Link href="/">Homepage</Link></li>
+          <li className={styles.listItem} ><Link href="/product/1">Products</Link></li>
           <li className={styles.listItem}>Menu</li>
           <Image src='/img/logo.png' alt='' width="100px" height="100px" />
-          <li className={styles.listItem}><Link passHref="/orders/1">Orders</Link></li>
+          <li className={styles.listItem}><Link href="/orders/1">Orders</Link></li>
           <li className={styles.listItem}>Blog</li>
           <li className={styles.listItem}>Contact</li>
         </ul>
       </div>
       <div className={styles.item}>
         <div className={styles.cart} >
-          <Link passHref="/cart"><Image src="/img/cart.png" alt="" width="30px" height="30px"/></Link>
+          <Link href="/cart"><Image src="/img/cart.png" alt="" width="30px" height="30px"/></Link>
           <div className={styles.counter}>2</div>
         </div>
       </div>
