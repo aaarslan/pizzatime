bea39410d736e451c0633e4a4e36f06e3be8a7bb
Abdallah Arslan <aarslan@Abdallahs-MacBook-Pro.local>
2022-04-28 14:47:11 -0400

    first commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,37 @@
+import styles from "../styles/Navbar.module.css"
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.callButton}>
+          <Image src="/img/telephone.png" alt="" width="32" height="32"/>
+        </div>
+        <div className={styles.texts}>
+          <div className={styles.text}>Order Now!</div>
+          <div className={styles.text}>123 456 7890</div>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}><a href="/">Homepage</a></li>
+          <li className={styles.listItem} ><a href="/product/1">Products</a></li>
+          <li className={styles.listItem}>Menu</li>
+          <Image src='/img/logo.png' alt='' width="100px" height="100px" />
+          <li className={styles.listItem}><a href="/orders/1">Orders</a></li>
+          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem}>Contact</li>
+        </ul>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.cart} >
+          <a href="/cart"><Image src="/img/cart.png" alt="" width="30px" height="30px"/></a>
+          <div className={styles.counter}>2</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
